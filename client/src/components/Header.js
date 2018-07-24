f5946eb1c733c995c115e7e9ee74f462f525cf2f
NolanHughes <Nhughes987@gmail.com>
2018-07-23 21:54:27 -0500

    Add cart count icon when cart count is greater than 0.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux';
 
 import ProductSearch from '../containers/ProductSearch';
 import '../css/Header.css'
 
-const Header = () => (
+const Header = ({cart}) => (
+
   <header>
 	  <nav id="main-nav" className="ui grid middle aligned">
-	    <Link to='/' id={"nav-logo"} className="nav-element three wide column">Drumverb</Link>
+	    <Link to='/' id="nav-logo" className="nav-element three wide column">Drumverb</Link>
 	  	<ProductSearch />
 	  	<ul>
 	  		<li id="sell-product-li">
@@ -16,7 +18,8 @@ const Header = () => (
 			  	</Link>
 		  	</li>
 		  	<li id="cart-li">
-			  	<Link to='/cart' id="cart-link" className="nav-element four wide column">
+			  	<Link to='/cart' id="cart-link" className="nav-element four wide column">			  		
+			  		{cartProductCount(cart)}
 			  		<i className="shopping cart icon" id="shoping-cart-icon">
 			  		</i>
 			  		<div id='cart-icon-label'>
@@ -27,7 +30,22 @@ const Header = () => (
 	  	</ul>
 	  </nav>
   </header>
-
 )
 
-export default Header
+function cartProductCount(cart) {
+	if(cart.length > 0) {
+		return <div id="cart-count">
+			 		   {cart.length}
+			  	 </div>
+	} else {
+		return false
+	}
+}
+
+const mapStateToProps = (state) => {
+  return {
+    cart: state.cart
+  };
+};
+
+export default connect(mapStateToProps)(Header)
