a32199c48deb1de8202571e71a3f5d6750d010bc
NolanHughes <Nhughes987@gmail.com>
2018-07-23 23:16:02 -0500

    Add buy button to cart and put in a dummy alert.

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -3,6 +3,7 @@ import React from 'react';
 import EmptyCart from './EmptyCart'
 import CartProductRows from './CartProductRows'
 import CartSumTable from './CartSumTable'
+import BuyButton from './BuyButton'
 import '../css/Cart.css'
 
 const Cart = ({ cart, removeProductFromCart }) => {
@@ -13,12 +14,18 @@ const Cart = ({ cart, removeProductFromCart }) => {
                     <EmptyCart />
                   </div>
   } else {
-    cartSection = <div id="cart-item-section" className="ui container">
-                    <h3 id="cart-label">Cart</h3>
-                    <CartProductRows cart={cart} />
-                    <hr id="table-separator"/>
-                    <CartSumTable cart={cart} />
+    cartSection = <div>
+                    <div id="cart-item-section" className="ui container">
+                      <h3 id="cart-label">Cart</h3>
+                      <CartProductRows cart={cart} />
+                      <hr id="table-separator"/>
+                      <CartSumTable cart={cart} />                     
+                    </div>
+                    <div className="ui container" id="buy-container">
+                      <button onClick={() => alert("Hello There")}>Buy</button>
+                    </div>
                   </div>
+                  
   }
 
   return (
