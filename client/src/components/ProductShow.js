18b6dfb42d0a60368fd8b291fa0fa1b1291e9af2
NolanHughes <Nhughes987@gmail.com>
2018-07-24 23:18:52 -0500

    Add search-overview section with listings count and dropdown menu for sorting. Set up function in React instead of Redux setting sortValue state.

--- client/src/components/ProductShow.js
+++ client/src/components/ProductShow.js
@@ -9,12 +9,13 @@ const ProductShow = ({ product, addProductToCart }) =>
   <div id="product-show" className="col-md-8 ui container">
     <h2>{product.title}</h2>
     <p>{product.description}</p>
+    <p>{product.category}</p>
     <button onClick={() => addProductToCart(product)}>Add to cart</button>
   </div>;
  
 const mapStateToProps = (state, ownProps) => {
   const product = state.products.find(product => product.id === parseInt(ownProps.match.params.productId, 10))
-
+  
   if (product) {
     return { 
       product
--- client/src/containers/ProductsPage.js
+++ client/src/containers/ProductsPage.js
@@ -6,13 +6,47 @@ import ProductIndexDisplay from '../components/ProductIndexDisplay';
 import Intro from '../components/Intro'
 
 class ProductsPage extends React.Component {  
+  constructor() {
+    super()
+
+    this.state = {
+      sortValue: 'select'
+    }
+  }
+
+  change = (event) => {
+    debugger
+    this.setState({sortValue: event.target.value})
+  }
+
   render() {
     const { products } = this.props
 
     return (
       <div>
         <Intro />      
-        <div className='ui container' id="switch-div">
+
+        <div className='ui container' id="switch-div">    
+          {/*Make sure it displays sorted products count and not all */}
+          <div className="search-overview">
+            <div className="search-overview_primary">
+              <h2 className="product-count">{products.length} listings</h2>     
+            </div> 
+            <div className="search-overview_secondary ui grid">
+              <div className="twelve wide column" id="filtered-by">
+                Filtered by:
+              </div>
+              <div className="four wide column" id="sort-dropdown">
+                {/*Map value to props. Make change function a prop and set state in Redux*/}
+                <select name="sort" id="sort" onChange={this.change} value={this.state.sortValue}>
+                  <option value="published_at|desc">Most Recent First</option>
+                  <option value="price|asc">Price Low to High</option>
+                  <option value="price|desc">Price High to Low</option>
+                </select>
+              </div>   
+            </div> 
+          </div>
+
           <Switch>
             <Route 
               path='/' 
