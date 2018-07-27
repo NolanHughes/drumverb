27d9884150bc1c7c9b21019d4f723235a7d010ca
NolanHughes <Nhughes987@gmail.com>
2018-07-26 19:10:02 -0500

    Use if/else statement to not allow add product to cart button to exist after product is already added to cart.

--- NOTES.md
+++ NOTES.md
@@ -2,7 +2,6 @@
 	-Have this post to api (Add users so the functionality works)(ignore this) 
 	-Make alert when products are added to cart
 		-Done except it needs to be refactored and maybe have background color overlay
-	-Dont have multiple instances of same product. Set it so in same session you can't add same product twice. Create an “error” or real error message saying you can't do that and disallow users from actually adding it again.
 
 2. Set up querying and filtering that can be implemented like the real website.
 	-Have sidebar and filtered_by for filtering
--- client/src/components/ProductShow.js
+++ client/src/components/ProductShow.js
@@ -6,54 +6,68 @@ import { addProductToCart } from '../actions/index';
 
 import '../css/ProductShow.css'
  
-const ProductShow = ({ product, addProductToCart }) =>
-  <div id="product-show" className="col-md-8 ui container">
+const ProductShow = ({ product, addProductToCart, cart }) =>{
+  let addToCartButton 
+  // debugger
+  if(cart.includes(product)) {
+    addToCartButton = <div>This product is already in your cart</div>   
+  } else {
+    addToCartButton = <button onClick={() => { addProductToCart(product); popUp(product) }}>Add to cart</button>
+  }
 
-    <div id="confirmBox">
-      <button className="ui icon button" id="pop-up-close-button" onClick={() => closePopUp()}>
-        <i className="window close outline icon"></i>
-      </button>      
-      <div className="message"><i className="check icon"></i>Item Added to Cart</div>  
-      <div id="pop-up-body"> 
-        <div className="ui grid row cart-row" key={product.id}>
-          <div className="three wide column">
-            <Link to={`/products/${product.id}`}>
-              <img className="pop-up-image" src={product.image_url} alt={product.title}/>
-            </Link>
-          </div>
-          <div className="eight wide column" id="pop-up-product-title">
-            <p>{product.title}</p>
-          </div>
-          <div className="five wide column pop-up-right-column">
-            <p>${product.price}.00</p>
-            <Link to='/cart'><button id="pop-up-cart-button">View Cart</button></Link>
-          </div>
-        </div> 
-      </div>  
-      
+  return(
+    <div id="product-show" className="col-md-8 ui container">
+  
+      <div id="confirmBox">
+        <button className="ui icon button" id="pop-up-close-button" onClick={() => closePopUp()}>
+          <i className="window close outline icon"></i>
+        </button>      
+        <div className="message"><i className="check icon"></i>Item Added to Cart</div>  
+        <div id="pop-up-body"> 
+          <div className="ui grid row cart-row" key={product.id}>
+            <div className="three wide column">
+              <Link to={`/products/${product.id}`}>
+                <img className="pop-up-image" src={product.image_url} alt={product.title}/>
+              </Link>
+            </div>
+            <div className="eight wide column" id="pop-up-product-title">
+              <p>{product.title}</p>
+            </div>
+            <div className="five wide column pop-up-right-column">
+              <p>${product.price}.00</p>
+              <Link to='/cart'><button id="pop-up-cart-button">View Cart</button></Link>
+            </div>
+          </div> 
+        </div>  
+        
+      </div>
+  
+      <h2>{product.title}</h2>
+      <p>{product.description}</p>
+      <p>{product.category}</p>
+      {addToCartButton}
     </div>
-
-    <h2>{product.title}</h2>
-    <p>{product.description}</p>
-    <p>{product.category}</p>
-    <button onClick={() => { addProductToCart(product); popUp(product) }}>Add to cart</button>
-  </div>;
+    )
+}
  
 const mapStateToProps = (state, ownProps) => {
   const product = state.products.find(product => product.id === parseInt(ownProps.match.params.productId, 10))
   
   if (product) {
     return { 
-      product
+      product,
+      cart: state.cart
     }
   } else {
-    return { product: {} }
+    return { 
+      product: {},
+      cart: []
+    }
   }
 }
 
 function popUp(product) {
   const confirmBox = document.getElementById("confirmBox");
-  debugger
   confirmBox.style.display = "block"
 }
 
