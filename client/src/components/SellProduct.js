625cb2ad60fe01ecd2d865852c127ffcd1103cd1
NolanHughes <Nhughes987@gmail.com>
2018-07-28 12:03:23 -0500

    Start styling sell page.

--- client/src/components/SellProduct.js
+++ client/src/components/SellProduct.js
@@ -10,12 +10,60 @@ class SellProduct extends React.Component {
     const { addNewProduct } = this.props
     
     return (
-      <div id="new-product">
-        <input ref="title" placeholder="Enter the title of the product" />
-        <input ref="description" placeholder="Enter a description" />
-        <input ref="price" placeholder="Enter a price" />
-        <input ref="image_url" placeholder="Enter an image url" />
-        <button onClick={() => addNewProduct(this)}>Submit</button>
+
+      <div id="new-product" className="ui two column centered grid">
+        <div className="column">
+          <h1>Sell on Reverb. No listing fees. Reach millions of musicians.</h1>
+        </div>
+        <div className="two column centered row">
+          <div className="column listing-editor">
+            <div className="listing-header">
+              <h3>What are you selling?</h3>
+            </div>
+            <form id="new-product-form">
+              <div className='ui grid'>
+                <div className="six wide column">
+                  <label>Brand*</label>
+                  <input ref="brand" />
+                  <p>e.g. Tama</p>
+                </div>
+                <div className="ten wide column">
+                  <label>Model*</label>
+                  <input ref="model" />
+                  <p>e.g. Starclassic</p>
+                </div>
+              </div>
+
+              <div className='ui grid'>
+                <div className="five wide column">
+                  <label>Condition*</label>
+                  <select id="product-condition">
+                    <option value="">Select Condition</option>
+                    <option value=""></option>
+                    <option value="brand-new">Brand New</option>
+                    <option value="mint">Mint</option>
+                    <option value="excellent">Excellent</option>
+                    <option value="very-good">Very Good</option>
+                    <option value="good">Good</option>
+                    <option value="fair">Fair</option>
+                    <option value="poor">Poor</option>
+                    <option value="non-functioning">Non Functioning</option>
+                  </select>
+                </div>
+                <div className="five wide column">
+                  <label>Year</label>
+                  <input ref="year" />
+                </div>
+                <div className="five wide column">
+                  <label>Finish</label>
+                  <input ref="finish" />
+                </div>
+              </div>
+              
+
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
@@ -28,3 +76,7 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 export default connect(null, mapDispatchToProps)(SellProduct);
+
+              // <input ref="price" placeholder="Enter a price" />
+              // <input ref="image_url" placeholder="Enter an image url" />
+              // <button onClick={() => addNewProduct(this)}>Submit</button>
