7dedd66e3dcfe72c11123574583b0d154f01d3ad
NolanHughes <Nhughes987@gmail.com>
2018-08-01 13:59:48 -0500

    Modify Routes.js so that it renders TestNewProduct component.

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -2,18 +2,19 @@ import React from 'react';
 import { Switch, Router, Route } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
 
-import BookIndex from './Book/Index';
-import BookNew from './Book/New';
-import BookEdit from './Book/Edit';
+// import BookIndex from './Book/Index';
+import TestNewProduct from './components/TestNewProduct';
+// import BookEdit from './Book/Edit';
 // import NotFound from './NotFound';
 
 const history = createBrowserHistory();
 const Routes = () =>
   <Router history={history}>
     <Switch>
-      <Route path="/books/:id/edit" component={BookEdit} />
-      <Route path="/books/new" component={BookNew} />
-      <Route path="/books/" component={BookIndex} />
+      {/*<Route path="/books/:id/edit" component={BookEdit} />*/}
+      {/*<Route path="/books/" component={BookIndex} />*/}
+      <Route path="/test-product" component={TestNewProduct} />
+    {/*<Route path="*" component={NotFound} />*/}
     </Switch>
   </Router>;
 
