534f6e5b57ec37e8daf2d669d27ebf236e329c79
NolanHughes <Nhughes987@gmail.com>
2018-08-01 13:41:25 -0500

    Update notes and product_image model, install axios, and add Routes.js to client folder.

---
+++ client/src/Routes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Switch, Router, Route } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
+
+import BookIndex from './Book/Index';
+import BookNew from './Book/New';
+import BookEdit from './Book/Edit';
+// import NotFound from './NotFound';
+
+const history = createBrowserHistory();
+const Routes = () =>
+  <Router history={history}>
+    <Switch>
+      <Route path="/books/:id/edit" component={BookEdit} />
+      <Route path="/books/new" component={BookNew} />
+      <Route path="/books/" component={BookIndex} />
+    </Switch>
+  </Router>;
+
+export default Routes;
